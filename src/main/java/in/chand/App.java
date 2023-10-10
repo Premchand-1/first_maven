65bb4f91a43be456db4eb8c205fdbcfacad69ecb
Premchand <chanducherry642@gmail.com>
2023-10-10 11:08:07 +0530

    this is my frist maven project

---
+++ src/main/java/in/chand/App.java
@@ -0,0 +1,13 @@
+package in.chand;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
